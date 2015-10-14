247b9ce2df465f1a2069275defb29864b1dcccd4
fzani <fzani@msn.com>
2015-10-14 20:42:38 -0300

    Start Application

---
+++ PlanetDatabase.Model/Entity/Star.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace PlanetDatabase.Model
+{
+    public class Star : IEntityBase
+    {   
+        public Star()
+        {
+            Planets = new List<Planet>();
+        }
+
+        //[Description("solar system")]
+        //SolarSystem,
+
+        //[Description("Alpha Centauri")]
+        //AlphaCentauri
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public virtual List<Planet> Planets { get; set; }
+    }
+}
---
+++ PlanetDatabase.UnitTestProject/PlanetDatabase.Model.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlanetDatabase.Model;
+
+namespace PlanetDatabase.UnitTestProject
+{
+    [TestClass]
+    public class PlanetDatabaseModelUnitTest
+    {
+        [TestMethod]
+        public void TestInstanceDatabaseContext()
+        {
+            using (var db = new PlanetDatabaseContext())
+            {
+                var planetList = db.Planets.ToList();
+
+                Assert.IsTrue(planetList != null);
+            }
+        }
+    }
+}
