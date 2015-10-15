c6b027c3272b77c6a75586338fe3f86c7aa018c7
fzani <fzani@msn.com>
2015-10-14 23:43:17 -0300

    tracked files

---
+++ PlanetDatabase.Business/PlanetBusiness.cs
@@ -0,0 +1,41 @@
+﻿using PlanetDatabase.Infrastructure.Base;
+using PlanetDatabase.Infrastructure.Repositories;
+using PlanetDatabase.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetDatabase.Business
+{
+    public class PlanetBusiness : IPlanetBusiness
+    {
+        private readonly PlanetRepository<Planet, UnitOfWork<PlanetDatabaseContext>> _repository;
+
+        public PlanetBusiness()
+        {
+            this._repository = new PlanetRepository<Planet, UnitOfWork<PlanetDatabaseContext>>();
+        }
+
+        public List<Planet> GetPlanets()
+        {
+            //throw new NotImplementedException();
+            return _repository.GetAll().ToList();
+        }
+
+
+        public List<Model.Planet> SetPlanet(Model.Planet planet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Model.Planet> GetPlanet(int planetId)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
---
+++ PlanetDatabase.Infrastructure/Repositories/PlanetRepository.cs
@@ -0,0 +1,17 @@
+﻿using PlanetDatabase.Infrastructure.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetDatabase.Infrastructure.Repositories
+{
+    public class PlanetRepository<T, U> : Repository<T>
+        where U : IUnitOfWork, new()
+        where T : class
+    {
+        public PlanetRepository() : base(new U()) { }
+       
+    }
+}
