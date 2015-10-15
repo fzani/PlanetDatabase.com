cfe210f69018c702a0654f376d4a5ee8ef9c79c6
fzani <fzani@msn.com>
2015-10-14 22:31:20 -0300

    Implement Planet Business And UnitTest

---
+++ PlanetDatabase.Business/IPlanetBusiness.cs
@@ -0,0 +1,19 @@
+﻿using PlanetDatabase.Infrastructure.Base;
+using PlanetDatabase.Infrastructure.Repositories;
+using PlanetDatabase.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlanetDatabase.Business
+{
+    interface IPlanetBusiness
+    {
+        List<Planet> GetPlanets();
+
+        List<Planet> SetPlanet(Planet planet);
+
+        List<Planet> GetPlanet(int planetId);
+    }
+}
---
+++ PlanetDatabase.UnitTestProject/PlanetDatabase.Business/PlanetBusinessTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlanetDatabase.Business;
+
+namespace PlanetDatabase.UnitTestProject.PlanetDatabase.Business
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class PlanetBusinessTest
+    {
+        private readonly PlanetBusiness _pb;
+        public PlanetBusinessTest()
+        {
+            _pb = new PlanetBusiness();
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var planetList = _pb.GetPlanets();
+
+            Assert.IsTrue(planetList != null);
+        }
+    }
+}
